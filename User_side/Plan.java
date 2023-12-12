04183f79a51ebfc6a8addfadf97308bc99cf8e6d
Ali Amgad <129622410+aliamgad@users.noreply.github.com>
2023-12-13 00:07:09 +0200

    Update Plan

--- User_side/Plan.java
+++ User_side/Plan.java
@@ -1,11 +1,45 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Calendar;
 public class Plan {
-    private String plan;
 
-    public void setPlan(String plan) {
-        this.plan = plan.toLowerCase();
+    /* Attributes */
+    int numberOfMovies;        // Maximum Number of movies allowed to the user according to the plan type
+    String typeOfPlan;               // The chosen plan
+    Calendar StartDate;
+    // A counter for each plan subscribed
+    // will be accessed by the admin to see the most subscribed plan among
+
+
+    /* End of Attributes*/
+
+    public void setTypeOfPlan(String typeOfPlan)
+    {
+        this.typeOfPlan = typeOfPlan;
+        checkPlanConstraints(this.typeOfPlan);
     }
 
-    public String getPlan() {
-        return plan;
+    public void checkPlanConstraints(String plan)
+    {
+        if(plan.equals("Basic"))
+        {
+            StartDate = Calendar.getInstance();
+            numberOfMovies= 5;
+        }
+        else if(plan.equals("Standard"))
+        {
+            StartDate = Calendar.getInstance();
+            numberOfMovies= 10;
+        }
+        else if(plan.equals("Premium"))
+        {
+            StartDate = Calendar.getInstance();
+            numberOfMovies= 30;
+        }
+        else
+        {
+            StartDate = null;
+            numberOfMovies =0;
+        }
     }
 }
